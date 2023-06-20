2e99eb4226b8ceeebde3b522c45a4d2d0f476a0e
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-20 20:03:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function volume_sphere() {
    let radius_value= document.getElementById("radius").value;
 	let volume = (4/3) * Math.PI * Math.pow(radius_value, 3);
-	document.getElementById("volume").value = volume;
+	return (document.getElementById("volume").value = volume);
 	
 } 
 
